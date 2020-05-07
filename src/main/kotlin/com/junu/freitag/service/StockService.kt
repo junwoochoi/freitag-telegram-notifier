432ffdf56e023ecd80c0df80da8a1112d16e223c
최준우 <junwoochoi@zuminternet.com>
2020-05-07 14:21:25 +0900

    refactor : create dao and refactor

--- src/main/kotlin/com/junu/freitag/service/StockService.kt
+++ src/main/kotlin/com/junu/freitag/service/StockService.kt
@@ -1,30 +1,20 @@
 package com.junu.freitag.service
 
 import com.junu.freitag.client.FreitagApiClient
+import com.junu.freitag.dao.StockDao
 import com.junu.freitag.entity.Product
-import com.junu.freitag.entity.ProductStock
-import com.junu.freitag.table.ProductStocks
-import org.jetbrains.exposed.sql.*
-import org.springframework.boot.context.event.ApplicationReadyEvent
-import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 
 @Component
-class StockService(private val freitagApiClient: FreitagApiClient) {
-
-//    @EventListener(ApplicationReadyEvent::class)
-//    @Transactional
-//    fun test() {
-//        val product = Product.new {
-//            productName = "F343_01431 CASE FOR IPHONE® XS/X"
-//            productId = "16863"
-//        }
-//    }
+class StockService(
+        private val freitagApiClient: FreitagApiClient,
+        private val stockDao: StockDao
+) {
 
     @Transactional
     fun refreshStocks() {
-        val allProducts = Product.all()
+        val allProducts = Product.all().toList()
 
         for (product in allProducts) {
             retrieveStocks(product)
@@ -33,45 +23,23 @@ class StockService(private val freitagApiClient: FreitagApiClient) {
 
     private fun retrieveStocks(product: Product) {
         val stocks = freitagApiClient.findStocks(product.productId)
-        val stockIds = stocks.products.map {
+        val searchedIds = stocks.products.map {
             it.product.productId
         }
+        stockDao.findSoldOutStocks(searchedIds)
+                .forEach { it.soldOut() }
 
-        val soldoutStocks = ProductStock.find {
-            ProductStocks.stockId notInList (stockIds) and
-                    (ProductStocks.exists eq true)
+        val exists = stockDao.findMarkedExists()
+
+        stocks.products.filter { wrapper ->
+            !exists.any { it.stockId == wrapper.product.productId }
+        }.map {
+            stockDao.create(
+                    stockId = it.product.productId,
+                    color = it.product.neoProductColors.trim(),
+                    imageUrl = it.product.neoProductCoverPhoto.src,
+                    product = product)
         }
-
-        for (soldoutStock in soldoutStocks) {
-            soldoutStock.soldOut()
-        }
-
-        val exists = ProductStock.find { ProductStocks.exists eq true }.notForUpdate()
-//        val exists = ProductStock.wrapRows(ProductStocks.select { ProductStocks.exists eq true }).toList()
-
-
-        for (i in listOf(1,2,3,4,5,6)) {
-            exists.forEach {
-                println(it.stockId)
-                println(it.javaClass)
-            }
-            println("----")
-        }
-//        stocks.products
-//                .forEach { stock ->
-//                    exists.forEach {
-//                        println(it.stockId)
-//                    }
-//                    println("----")
-//                }
-//    }
-//                .forEach {
-//                    ProductStock.new {
-//                        imageUrl = it.product.neoProductCoverPhoto.src
-//                        color = it.product.neoProductColors.trim()
-//                        stockId = it.product.productId
-//                    }
-//                }
 
 
     }
--- src/main/kotlin/com/junu/freitag/table/Stocks.kt
+++ src/main/kotlin/com/junu/freitag/table/Stocks.kt
@@ -1,8 +1,9 @@
 package com.junu.freitag.table
 
-object ProductStocks : BaseTable() {
+object Stocks : BaseTable() {
     val stockId = varchar("stock_id", 30)
     val color = varchar("color", 50)
     val imageUrl = varchar("image_url", 300)
     val exists = bool("exists").default(true)
+    val product = reference("product", Products)
 }
